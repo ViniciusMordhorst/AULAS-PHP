5c339d46f331b519a8915fbb5349462cc1a7aae3
Vinicius Kahler Mordhorst <99055726+ViniciusMordhorst@users.noreply.github.com>
2024-08-26 14:55:24 -0300

    Delete aula06 directory

--- aula06/includes/banco de dados.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE ifpr;
-USE ifpr;
-CREATE TABLE usuario (
-    id INT NOT NULL AUTO_INCREMENT,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    senha VARCHAR(255) NOT NULL,
-    PRIMARY KEY(id)
-);
