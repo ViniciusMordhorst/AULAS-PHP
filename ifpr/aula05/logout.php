1a05126a21569a4e9bab2ebd42e50e91aaf33f20
Vinicius Kahler Mordhorst <99055726+ViniciusMordhorst@users.noreply.github.com>
2024-09-12 20:16:19 -0300

    Delete ifpr directory

--- ifpr/aula05/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-
-    $_SESSION['usuario'] = '';
-    
-    session_destroy();
-    header('Location: login.php');
-?>
--- ifpr/aula07/includes/conexao.inc.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    $bancoDados = NULL;
-
-    try {
-        $bancoDados = new PDO("mysql:host=127.0.0.1;port=33306;dbname=ifpr","root","");
-
-    } catch (\Throwable $th) {
-        exit("Erro na conexão com o banco de dados.");
-    }
